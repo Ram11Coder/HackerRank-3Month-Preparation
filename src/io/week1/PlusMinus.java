f805b2214838934dbe16210a59c1db2fc96b470a
Ram11Coder <rambeliver11@gmail.com>
2022-10-06 23:32:51 +0530

    initial commit

---
+++ src/io/week1/PlusMinus.java
@@ -0,0 +1,57 @@
+package io.week1;
+
+/*
+public class PlusMinus {
+}
+*/
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'plusMinus' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void plusMinus(List<Integer> arr) {
+        int pos = 0, neg = 0, zero = 0;
+        for (int i : arr) {
+            if (i > 0)
+                pos++;
+            else if (i < 0)
+                neg++;
+            else
+                zero++;
+        }
+        double n = (double) arr.size();
+        System.out.printf("%.6f\n%.6f\n%.6f\n", pos / n, neg / n, zero / n);
+    }
+
+}
+
+public class PlusMinus {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+        Result.plusMinus(arr);
+        bufferedReader.close();
+    }
+}
