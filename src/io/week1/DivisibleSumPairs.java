d85aca1233d9bdc50e89a2b08e7c64545b88a459
Ram11Coder <rambeliver11@gmail.com>
2022-10-07 20:14:45 +0530

    modified: included optimised solution

--- src/io/week1/DivisibleSumPairs.java
+++ src/io/week1/DivisibleSumPairs.java
@@ -37,6 +37,20 @@ public class DivisibleSumPairs {
         return pair;
     }
 
+    // O(n) solution using map
+    public static int divisibleSumPairs_Optimised(int n, int k, List<Integer> arr) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+        int pairCount = 0;
+        for (int i : arr) {
+            int mod = i % k; // Apply module to track the elements divisible by k
+            int comp = (mod == 0) ? 0 : k - mod;
+            pairCount += (map.containsKey(comp)) ? map.get(comp) : 0;// If element present return value otherwise 0
+            map.put(mod, (map.containsKey(mod)) ? map.get(mod) + 1 : 1);//Adding to the map already traversed element and incrementing
+        }
+        return pairCount;
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         //BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
@@ -52,6 +66,8 @@ public class DivisibleSumPairs {
                 .collect(toList());
 
         int result = DivisibleSumPairs.divisibleSumPairs(n, k, ar);
+        System.out.println(result);
+        result = DivisibleSumPairs.divisibleSumPairs_Optimised(n, k, ar);
         System.out.println(result);
         //bufferedWriter.write(String.valueOf(result));
         // bufferedWriter.newLine();
