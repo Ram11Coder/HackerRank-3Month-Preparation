0cee99da412e67af2532345e472ad1867c91251b
Ram11Coder <rambeliver11@gmail.com>
2022-10-09 20:58:00 +0530

    Added: Week2 - Flipping bits solutions

---
+++ src/io/week2/Flippingbits.java
@@ -0,0 +1,55 @@
+package io.week2;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+public class Flippingbits {
+
+    /*
+     * Complete the 'flippingBits' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts LONG_INTEGER n as parameter.
+     *
+     * Integer.toUnsignedLong(int ) -> converts the signed integer into unsigned int
+     */
+
+    public static long flippingBits(long n) {
+
+        return Integer.toUnsignedLong((int) ~n);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        // BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                long n = Long.parseLong(bufferedReader.readLine().trim());
+
+                long result = Flippingbits.flippingBits(n);
+                System.out.println(result);
+                //   bufferedWriter.write(String.valueOf(result));
+                //   bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        // bufferedWriter.close();
+    }
+}
