44178f3f2152abc9d2c546d78b3b2f19772158d4
Ram11Coder <rambeliver11@gmail.com>
2022-10-11 21:04:52 +0530

    Added: Week3 - XOR problem

---
+++ src/io/week3/XORStrings3.java
@@ -0,0 +1,37 @@
+package io.week3;
+
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class XORStrings3 {
+    //Alternate way to check XOR operation
+    public static String stringsXOR(String s, String t) {
+        String res = "";
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == t.charAt(i))
+                res += "0";
+            else
+                res += "1";
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+
+        String s, t;
+        Scanner in = new Scanner(System.in);
+        s = in.nextLine();
+        t = in.nextLine();
+        System.out.println(stringsXOR(s, t));
+
+    }
+
+}
+
+
+
