ce3ff3be7f07f8dc6c69f7e5f0733a9fdd49cad9
Ram11Coder <rambeliver11@gmail.com>
2022-10-25 23:55:03 +0530

    Added MaxMin solution

--- src/io/week5/MaxMin.java
+++ src/io/week5/MaxMin.java
@@ -1,4 +1,65 @@
 package io.week5;
 
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
 public class MaxMin {
+    /*
+     * Complete the 'maxMin' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER k
+     *  2. INTEGER_ARRAY arr
+     */
+
+    public static int maxMin(int k, List<Integer> arr) {
+        Collections.sort(arr);
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i <= arr.size() - k; i++)
+            if (arr.get(i + k - 1) - arr.get(i) < min)
+                min = arr.get(i + k - 1) - arr.get(i);
+
+        return min;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        // BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int k = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = IntStream.range(0, n).mapToObj(i -> {
+            try {
+                return bufferedReader.readLine().replaceAll("\\s+$", "");
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        })
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = MaxMin.maxMin(k, arr);
+        System.out.println(result);
+        //  bufferedWriter.write(String.valueOf(result));
+        //  bufferedWriter.newLine();
+
+        bufferedReader.close();
+        //  bufferedWriter.close();
+    }
 }
