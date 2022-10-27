a5a8edf6401205066f45f9299b1880e97555a68a
Ram11Coder <rambeliver11@gmail.com>
2022-10-27 23:56:17 +0530

    Added Missing Numbers solution

--- src/io/week5/MissingNumbers.java
+++ src/io/week5/MissingNumbers.java
@@ -1,4 +1,72 @@
 package io.week5;
 
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
 public class MissingNumbers {
+    /*
+     * Complete the 'missingNumbers' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY arr
+     *  2. INTEGER_ARRAY brr
+     */
+
+    public static List<Integer> missingNumbers(List<Integer> arr, List<Integer> brr) {
+        int freq[] = new int[10001];
+        List<Integer> result = new ArrayList<>();
+        for (int i : brr)
+            freq[i]++;
+
+        for (int i : arr)
+            freq[i]--;
+
+        for (int i = 1; i < freq.length; i++)
+            if (freq[i] > 0)
+                result.add(i);
+
+        return result;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        // BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int m = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> brr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = MissingNumbers.missingNumbers(arr, brr);
+        System.out.println(result);
+       /* bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining(" "))
+                        + "\n"
+        );*/
+
+        bufferedReader.close();
+        //bufferedWriter.close();
+    }
 }
