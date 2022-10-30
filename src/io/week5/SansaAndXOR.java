267c13cb1079f92a53e389b77cd390e6e7ca5c3c
Ram11Coder <rambeliver11@gmail.com>
2022-10-30 15:44:52 +0530

    Added Sansa And XOR solution

--- src/io/week5/SansaAndXOR.java
+++ src/io/week5/SansaAndXOR.java
@@ -1,4 +1,63 @@
 package io.week5;
 
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
 public class SansaAndXOR {
+    /*
+     * Complete the 'sansaXor' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int sansaXor(List<Integer> arr) {
+        int n = arr.size();
+        if (n % 2 == 0)
+            return 0;
+        int sum = 0;
+        for (int i = 0; i < arr.size(); i += 2)
+            sum ^= arr.get(i);
+
+        return sum;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        //  BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                        .map(Integer::parseInt)
+                        .collect(toList());
+
+                int result = SansaAndXOR.sansaXor(arr);
+
+                //    bufferedWriter.write(String.valueOf(result));
+                //     bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        // bufferedWriter.close();
+    }
 }
