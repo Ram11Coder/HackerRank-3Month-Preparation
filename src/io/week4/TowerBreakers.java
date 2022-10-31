80a994c70a9d426d35179296307e7474c6d00bc0
Ram11Coder <rambeliver11@gmail.com>
2022-10-31 22:31:19 +0530

    Added Tower breaker solution

--- src/io/week4/TowerBreakers.java
+++ src/io/week4/TowerBreakers.java
@@ -26,9 +26,38 @@ public class TowerBreakers {
      *  2. INTEGER m
      */
 
+    /**
+     * Player p1
+     * Player p2
+     * <p>
+     * if there is even no of towers always p2 will win
+     * because first move taken by always player 1
+     * let's take example:
+     * Total Tower n=2 , Height m=9
+     * case 1:
+     * P1 -> Reduce Tower 1 height to 4 (9/2)
+     * p2 -> Reduce Tower 2 height to 4 (Because both player optimally move)
+     * <p>
+     * P1 -> Reduce Tower 1 height from 4 to 1
+     * P2 -> Reduce Tower 2 height from 4 to 1
+     * <p>
+     * now P1 does not have any move so p2 wins
+     * <p>
+     * case 2:
+     * P1 -> Reduce Tower 1 height from 9 to 1
+     * P2 -> Reduce Tower 2 height from 9 to 1
+     * now P1 does not have any move so p2 wins
+     * <p>
+     * Both scenarios p1 will win(So even no tower always p2 will win)
+     * <p>
+     * Scenarios : Odd no of tower
+     * ex: Total Tower n=1 , Height m=5
+     * Player 1 either reduce to 2 ro 1 , p2 can not move or optimally move
+     * so Player 1 will always win
+     * But if m=1 then p1 can not make any move so player 2 will win
+     */
     public static int towerBreakers(int n, int m) {
-        return 0;
-
+        return (n % 2 == 0 || m == 1) ? 2 : 1;
     }
 
     public static void main(String[] args) throws IOException {
@@ -46,7 +75,7 @@ public class TowerBreakers {
                 int m = Integer.parseInt(firstMultipleInput[1]);
 
                 int result = TowerBreakers.towerBreakers(n, m);
-
+                System.out.println(result);
                 //      bufferedWriter.write(String.valueOf(result));
                 //        bufferedWriter.newLine();
             } catch (IOException ex) {
