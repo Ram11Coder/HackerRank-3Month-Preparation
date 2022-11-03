b0f0ed5b5bc97938648484f8f201772c85d3fa08
Ram11Coder <rambeliver11@gmail.com>
2022-11-03 23:56:53 +0530

    Added SumVsXOR solution

--- src/io/week6/SumVsXOR.java
+++ src/io/week6/SumVsXOR.java
@@ -24,18 +24,35 @@ public class SumVsXOR {
      * The function accepts LONG_INTEGER n as parameter.
      */
 
-    public static long sumXor(long n) {
-        return 0;
+    public static long sumXor_optimised(long n) {
+        int c=0;
+        while (n > 0) {
+            if (n % 2 == 0)
+                c++;
+            n/= 2;
+        }
+        return (long) Math.pow(2,c);
 
     }
 
+//Simplified the code
+    public static long sumXor(long n) {
+        long res=1;
+        while (n > 0) {
+            if ((n & 1) == 0)
+                res*=2;
+            n/= 2;
+        }
+        return res;
+
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         // BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
         long n = Long.parseLong(bufferedReader.readLine().trim());
 
-        long result = SumVsXOR.sumXor(n);
+        long result = SumVsXOR.sumXor_optimised(n);
         System.out.println(result);
         //  bufferedWriter.write(String.valueOf(result));
         //    bufferedWriter.newLine();
